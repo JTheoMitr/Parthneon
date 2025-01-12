b8aa45a5a4fbc2a86a7dfaebe74667e83f0aecd1
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2025-01-11 23:49:55 -0600

    enemies award 15 pts now
    enemy bot 1 and 2 but not boss drones

--- enemies/neon_bot_enemy_2.gd
+++ enemies/neon_bot_enemy_2.gd
@@ -5,11 +5,14 @@ extends Area2D
 @onready var anim2 = $AnimatedSprite2D2
 @onready var explosion = $AnimatedSprite2D3
 @onready var bloop = $AudioStreamPlayer
+@onready var points = $RichTextLabel
+@onready var stats = PlayerStats
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("default")
 	anim2.play("default")
 	explosion.hide()
+	points.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -30,7 +33,14 @@ func _on_area_entered(_area: Area2D) -> void:
 	anim.hide()
 	anim2.hide()
 	bloop.play(0.0)
+	points.show()
+	$DeathTimer.start()
+	stats.score += 15
 
 
 func _on_animated_sprite_2d_3_animation_finished() -> void:
+	self.queue_free()
+
+
+func _on_death_timer_timeout() -> void:
 	self.queue_free()
