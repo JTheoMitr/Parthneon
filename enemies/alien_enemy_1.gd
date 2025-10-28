71a218bfa1832376a52697571edbe8cca0901dbf
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2025-10-28 00:28:04 -0500

    Alien life updates in space lab LV1
    playtest and add new audio bits

---
+++ enemies/alien_enemy_1.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+@export var speed = 2.0
+@onready var anim = $AnimatedSprite2D
+
+@onready var explosion = $AnimatedSprite2D3
+@onready var bloop = $AudioStreamPlayer
+@onready var points = $RichTextLabel
+@onready var stats = PlayerStats
+@onready var hitbox = $CollisionShape2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("default")
+
+	explosion.hide()
+	points.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	self.global_position.x -= speed
+	#self.rotation += .03
+	
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	self.queue_free()
+
+
+func _on_area_entered(_area: Area2D) -> void:
+	explosion.show()
+	explosion.play("default")
+	anim.hide()
+
+	bloop.play(0.0)
+	points.show()
+	$DeathTimer.start()
+	stats.score += 15
+	hitbox.queue_free()
+
+
+func _on_animated_sprite_2d_3_animation_finished() -> void:
+	explosion.hide()
+
+
+func _on_death_timer_timeout() -> void:
+	self.queue_free()
